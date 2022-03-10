8839c397490e1d35ec7cd7ac4f985654c04a3e6b
Brenda Guadalupe Sánchez Sánchez <98482060+brendasanchezs@users.noreply.github.com>
2022-03-10 14:10:04 -0600

    Add files via upload

---
+++ dags/sparkify_dag.py
@@ -0,0 +1,145 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'ashwath',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 11, 1),
+    'end_date': datetime(2018, 11, 30),
+    'email': ['ashwath92@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+
+# hourly: cron is '0 * * * *': https://airflow.apache.org/docs/stable/scheduler.html
+dag = DAG('sparkify_elt_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          max_active_runs=1,
+          # https://airflow.apache.org/docs/stable/scheduler.html
+          schedule_interval='0 0 * * *'
+          #schedule_interval=timedelta(days=1),
+          #schedule_interval='0 * * * *'
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+create_tables_task = PostgresOperator(
+    task_id='create_tables',
+    dag=dag,
+    #sql in same directory
+    sql='create_tables.sql',
+    postgres_conn_id='redshift'
+)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='udacity-dend',
+    s3_key='log_data',
+    region='us-west-2',
+    destination_table='staging_events',
+    input_file_type='json',
+    start_date=datetime(2018, 11, 1),
+    provide_context=True
+    
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='udacity-dend',
+    s3_key='song_data',
+    region='us-west-2',
+    destination_table='staging_songs',
+    input_file_type='json',
+    start_date=datetime(2018, 11, 1),
+    provide_context=True
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    sql=SqlQueries.songplay_table_insert,
+    provide_context=True
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    destination_table='users',
+    mode='append',
+    sql=SqlQueries.user_table_insert
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    destination_table='songs',
+    mode='truncate',
+    sql=SqlQueries.song_table_insert
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    destination_table='artists',
+    mode='truncate',
+    sql=SqlQueries.artist_table_insert
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    destination_table='time',
+    mode='append',
+    sql=SqlQueries.time_table_insert,
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn_id='redshift'
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> create_tables_task 
+create_tables_task >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+load_songplays_table >> [load_artist_dimension_table, load_song_dimension_table, load_user_dimension_table, load_time_dimension_table] >> run_quality_checks
+run_quality_checks >> end_operator
