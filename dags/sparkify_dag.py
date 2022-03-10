1579f08ccfb2fc2d986d8e2b5859209c8c59d74e
Brenda Guadalupe Sánchez Sánchez <98482060+brendasanchezs@users.noreply.github.com>
2022-03-10 14:45:43 -0600

    Update sparkify_dag.py

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -12,9 +12,6 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 
-
-from helpers import SqlQueries
-
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
@@ -235,8 +232,94 @@ class DataQualityOperator(BaseOperator):
                 self.log.error(f"Data quality check failed. {table} returned no results")
                 raise ValueError(f"Data quality check failed. {table} returned no results")
             self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
-###################################################
+###############################SQL QUERIES ####################
 
+class SqlQueries:
+    songplay_table_insert = ("""
+        INSERT INTO songplays (
+            play_id,
+            start_time,
+            user_id,
+            level,
+            song_id,
+            artist_id,
+            session_id,
+            location,
+            user_agent
+        )
+        SELECT
+                md5(events.sessionid || events.start_time) AS playid,
+                events.start_time, 
+                events.userid, 
+                events.level, 
+                songs.song_id, 
+                songs.artist_id, 
+                events.sessionid, 
+                events.location, 
+                events.useragent
+                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+            FROM staging_events
+            WHERE page='NextSong') events
+            LEFT JOIN staging_songs songs
+            ON events.song = songs.title
+                AND events.artist = songs.artist_name
+                AND events.length = songs.duration
+    """)
+
+    user_table_insert = ("""
+         INSERT INTO users (
+            user_id,
+            first_name,
+            last_name,
+            gender,
+            level
+        )
+        SELECT distinct userid, firstname, lastname, gender, level
+        FROM staging_events
+        WHERE page='NextSong'
+    """)
+
+    song_table_insert = ("""
+        INSERT INTO songs (
+            song_id,
+            title,
+            artist_id,
+            year,
+            duration
+        )
+        SELECT distinct song_id, title, artist_id, year, duration
+        FROM staging_songs
+    """)
+
+    artist_table_insert = ("""
+        INSERT INTO artists (
+            artist_id,
+            artist_name,
+            artist_location,
+            artist_latitude,
+            artist_longitude    
+        )
+        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+        FROM staging_songs
+    """)
+
+    time_table_insert = ("""
+        INSERT INTO time (
+            start_time,
+            hour,
+            day,
+            week,
+            month,
+            year,
+            weekday
+        )
+        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
+               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
+        FROM songplays
+    """)
+
+
+#############################################################
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
